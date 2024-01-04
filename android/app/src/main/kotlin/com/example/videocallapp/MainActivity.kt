1f861e611281c0284d39fc53131efe0f77960f4b
AhmedAbdelhakem <100963499+AhmedAbdelhakem@users.noreply.github.com>
2024-01-04 16:42:16 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/videocallapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.videocallapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<agora_rtc_engine/AgoraRtcEnginePlugin.h>)
+#import <agora_rtc_engine/AgoraRtcEnginePlugin.h>
+#else
+@import agora_rtc_engine;
+#endif
+
+#if __has_include(<permission_handler_apple/PermissionHandlerPlugin.h>)
+#import <permission_handler_apple/PermissionHandlerPlugin.h>
+#else
+@import permission_handler_apple;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AgoraRtcEnginePlugin registerWithRegistrar:[registry registrarForPlugin:@"AgoraRtcEnginePlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+}
+
+@end
